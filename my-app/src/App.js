ae442014388b664eb3c3b513d68e80ffae1d0ff8
vpardeshi419 <vpardeshi419@gmail.com>
2023-10-17 16:59:52 +0530

    Initial setup

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <Demo></Demo>
+    </div>
+  );
+}
+
+function Demo() {
+  return (
+    <div className="App">
+      demo
+      <Demo1></Demo1>
+    </div>
+  );
+}
+
+export default App;
